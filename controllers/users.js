91e6222a69ce3af93164cebe74e1fda025c81591
Isaac Terry <zeroice2007@gmail.com>
2021-08-23 21:23:07 -0500

    add ability for user to sign up or sign in

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.handleGetCurrentUser = (req, res, next) => {
@@ -18,6 +19,41 @@ module.exports.handleGetCurrentUser = (req, res, next) => {
         email: user.email,
         name: user.name,
       });
+    })
+    .catch(next);
+};
+
+module.exports.handleCreateUser = (req, res, next) => {
+  const { body } = req;
+  const SALT_LENGTH = 10;
+  bcrypt.hash(body.password, SALT_LENGTH)
+    .then((hashedPassword) => (
+      User.create({
+        email: body.email,
+        name: body.name,
+        password: hashedPassword,
+      })
+    ))
+    .then((user) => {
+      const RESOURCE_CREATED_STATUS_CODE = 201;
+      res.status(RESOURCE_CREATED_STATUS_CODE).send({
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      });
+    })
+    .catch((err) => {
+      const MONGO_DUPLICATION_ERROR_NAME = 'MongoError';
+      const MONGO_DUPLICATION_ERROR_CODE = 11000;
+
+      if (err.name !== MONGO_DUPLICATION_ERROR_NAME || err.code !== MONGO_DUPLICATION_ERROR_CODE) {
+        throw err;
+      }
+
+      const CONFLICT_STATUS_CODE = 209;
+      const newError = new Error('Must use a different email to create a new user.');
+      newError.httpStatusCode = CONFLICT_STATUS_CODE;
+      throw newError;
     })
     .catch(next);
 };
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const articlesRouter = require('./articles');
 const usersRouter = require('./users');
 const handleValidationErrors = require('../middleware/error/validation');
+const auth = require('../controllers/auth');
+const { handleCreateUser } = require('../controllers/users');
 
 const router = express.Router();
 
 router.use(express.json());
+
+router.post('/signin', auth);
+router.post('/signup', handleCreateUser);
+
 router.use('/articles', articlesRouter);
 router.use('/users', usersRouter);
+
 router.use(handleValidationErrors);
 
 module.exports = router;
